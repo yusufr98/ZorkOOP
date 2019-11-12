296a4054d56758a615e4aede7c911be2f8afb747
yusufr98 <yusuf090798@gmail.com>
2019-11-12 11:02:24 -0500

    initial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,63 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Random r = new Random();
+        Scanner k = new Scanner(System.in);
+        Room[][] rooms = new Room[4][4];
+        Room first = new Room(r.nextInt(4), r.nextInt(4));
+        rooms[first.getRow()][first.getCol()] = first;
+        for (int i = 0; i < 9; i++) {
+            boolean repeat = true;
+            int c, ro;
+            do {
+                c = r.nextInt(4);
+                ro = r.nextInt(4);
+                if (rooms[ro][c] != null) {
+                    continue;
+                }
+                if (ro == 3) {
+                    if (c == 0) {
+                        if (rooms[ro - 1][c] != null || rooms[ro][c + 1] != null) {
+                            repeat = false;
+                        }
+                    } else if (c == 3) {
+                        if (rooms[ro - 1][c] != null || rooms[ro][c - 1] != null) {
+                            repeat = false;
+                        }
+                    } else if (rooms[ro][c - 1] != null || rooms[ro - 1][c] != null || rooms[ro][c + 1] != null) {
+                        repeat = false;
+                    }
+                } else if (c == 3) {
+                    if (ro == 0) {
+                        if (rooms[ro][c - 1] != null || rooms[ro + 1][c] != null) {
+                            repeat = false;
+                        }
+                    } else if (rooms[ro][c - 1] != null || rooms[ro + 1][c] != null || rooms[ro - 1][c] != null) {
+                        repeat = false;
+                    }
+                } else if (ro == 0) {
+                    if (c == 0) {
+                        if (rooms[ro + 1][c] != null || rooms[ro][c + 1] != null) {
+                            repeat = false;
+                        }
+                    } else if (rooms[ro][c - 1] != null || rooms[ro + 1][c] != null || rooms[ro][c + 1] != null) {
+                        repeat = false;
+                    }
+                } else if (c == 0) {
+                    if (rooms[ro - 1][c] != null || rooms[ro][c + 1] != null || rooms[ro + 1][c] != null) {
+                        repeat = false;
+                    }
+                } else if (rooms[ro][c - 1] != null || rooms[ro - 1][c] != null || rooms[ro + 1][c] != null || rooms[ro][c + 1] != null) {
+                    repeat = false;
+                }
+            } while (repeat);
+            rooms[ro][c] = new Room(ro, c);
+        }
+            for (int i = 0; i < rooms.length; i++) {
+                System.out.println(Arrays.toString(rooms[i]));
+            }
     }
 }
--- src/Room.java
+++ src/Room.java
@@ -1,107 +1,33 @@
-package com.company;
+
 
 import java.util.Random;
 
 public class Room {
-    private String name;
-    private String item;
-    private int index;
-    private int money;
-    private int north, south, east , west;
-    Random r = new Random();
-    private boolean visit;
-    private boolean teddy;
+   private int row;
+   private int col;
 
-    Room(String name, int index, int north, int south, int east, int west) {
-        this.name = name;
-        this.index = index;
-        this.money = 1 + r.nextInt(1000);
-        this.north = north;
-        this.south = south;
-        this.east = east;
-        this.west = west;
-        visit = false;
-        teddy = false;
+    public Room() {
     }
 
-    public String getName() {
-        return name;
+    public Room(int row, int col) {
+        this.col = col;
+        this.row = row;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public int getCol() {
+        return col;
     }
 
-    public int getIndex() {
-        return index;
+    public void setCol(int col) {
+        this.col = col;
     }
 
-    public void setIndex(int index) {
-        this.index = index;
+    public int getRow() {
+        return row;
     }
 
-    public int getMoney() {
-        return money;
-    }
-
-    public void setMoney(int money) {
-        this.money = money;
-    }
-
-    public int getNorth() {
-        return north;
-    }
-
-    public void setNorth(int north) {
-        this.north = north;
-    }
-
-    public int getSouth() {
-        return south;
-    }
-
-    public void setSouth(int south) {
-        this.south = south;
-    }
-
-    public int getEast() {
-        return east;
-    }
-
-    public void setEast(int east) {
-        this.east = east;
-    }
-
-    public int getWest() {
-        return west;
-    }
-
-    public void setWest(int west) {
-        this.west = west;
-    }
-
-    public String getItem() {
-        return item;
-    }
-
-    public void setItem(String item) {
-        this.item = item;
-    }
-
-    public boolean isVisit() {
-        return visit;
-    }
-
-    public void setVisit(boolean visit) {
-        this.visit = visit;
-    }
-
-    public boolean isTeddy() {
-        return teddy;
-    }
-
-    public void setTeddy(boolean teddy) {
-        this.teddy = teddy;
+    public void setRow(int row) {
+        this.row = row;
     }
 }
 
