486f92fc2e429df7d9fd1fcb68d4a56e47b4f4b8
yusufr98 <yusuf090798@gmail.com>
2019-11-12 11:44:56 -0500

    hooray

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+    public static Room[][] rooms = new Room[4][4];
     public static void main(String[] args) {
         Random r = new Random();
         Scanner k = new Scanner(System.in);
-        Room[][] rooms = new Room[4][4];
-        Room first = new Room(r.nextInt(4), r.nextInt(4));
+        Room first = new Room(r.nextInt(4), r.nextInt(4), 1);
         rooms[first.getRow()][first.getCol()] = first;
         for (int i = 0; i < 9; i++) {
             boolean repeat = true;
@@ -54,10 +54,82 @@ public class Main {
                     repeat = false;
                 }
             } while (repeat);
-            rooms[ro][c] = new Room(ro, c);
+            rooms[ro][c] = new Room(ro, c, i+2);
         }
-            for (int i = 0; i < rooms.length; i++) {
-                System.out.println(Arrays.toString(rooms[i]));
-            }
+
+        System.out.println("This is a directional adventure game. The goal is to find the secret room. " +
+                "From your starting room in the foyer you can move in any direction using N,S, E, or W. Any other key will cause an error, good luck!");
+        System.out.println("Welcome to Zork! Type QUIT at any time to quit the game.");
+
+        boolean end = true;
+        Room current = first;
+        while(end){//moving through rooms, it ends if quit is entered
+            String d;
+            boolean repeat = true;
+            System.out.println("You are in Room " + current.getIndex() +"(" + current.getRow() + ", " + current.getCol() + ")");
+            do {//choose direction, it repeats if invalid direction was chosen
+                System.out.println("Which direction would you like to go in?");
+                d = k.next();
+                if (d.equalsIgnoreCase("n")) {
+                    if(current.getRow() == 0){
+                        System.out.println("Invalid direction");
+                    }
+                    else{
+                        if((rooms[current.getRow()-1][current.getCol()]) == null){
+                            System.out.println("Invalid direction");
+                        }
+                        else{
+                            Room temp = new Room(current.getRow()-1, current.getCol(), rooms[current.getRow()-1][current.getCol()].getIndex());
+                            current = temp;
+                            repeat = false;
+                        }
+                    }
+                } else if (d.equalsIgnoreCase("s")) {
+                    if(current.getRow() == 3){
+                        System.out.println("Invalid direction");
+                    }
+                    else{
+                        if((rooms[current.getRow()+1][current.getCol()]) == null){
+                            System.out.println("Invalid direction");
+                        }
+                        else{
+                            Room temp = new Room(current.getRow()+1, current.getCol(), rooms[current.getRow()+1][current.getCol()].getIndex());
+                            current = temp;
+                            repeat = false;
+                        }
+                    }
+                } else if (d.equalsIgnoreCase("w")) {
+                    if(current.getCol() == 0){
+                        System.out.println("Invalid direction");
+                    }
+                    else{
+                        if((rooms[current.getRow()][current.getCol()-1]) == null){
+                            System.out.println("Invalid direction");
+                        }
+                        else{
+                            Room temp = new Room(current.getRow(), current.getCol()-1, rooms[current.getRow()][current.getCol()-1].getIndex());
+                            current = temp;
+                            repeat = false;
+                        }
+                    }
+                } else if (d.equalsIgnoreCase("e")) {
+                    if(current.getCol() == 3){
+                        System.out.println("Invalid direction");
+                    }
+                    else{
+                        if((rooms[current.getRow()][current.getCol()+1]) == null){
+                            System.out.println("Invalid direction");
+                        }
+                        else{
+                            Room temp = new Room(current.getRow(), current.getCol()+1, rooms[current.getRow()][current.getCol()+1].getIndex());
+                            current = temp;
+                            repeat = false;
+                        }
+                    }
+                } else if (d.equalsIgnoreCase("quit")) {
+                    end = false;
+                }
+            } while (repeat);
+        }
     }
 }
--- src/Room.java
+++ src/Room.java
@@ -5,13 +5,23 @@ import java.util.Random;
 public class Room {
    private int row;
    private int col;
+   private int index;
 
     public Room() {
     }
 
-    public Room(int row, int col) {
+    public Room(int row, int col, int index) {
         this.col = col;
         this.row = row;
+        this.index = index;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
     }
 
     public int getCol() {
